1817eb496e3dd5957f6ba278a2983e5db3927980
Huff <jdhu233@uky.edu>
2026-02-24 16:58:27 -0500

    v1: ERC20 + staking vault with deploy script and helpers

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -1,2 +1,20 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
+
+import "forge-std/Script.sol";
+import "../src/MyToken.sol";
+import "../src/StakingVault.sol";
+
+contract Deploy is Script {
+    function run() external {
+        vm.startBroadcast(); // signer comes from CLI (keystore/ledger/etc.)
+
+        MyToken token = new MyToken("MyToken", "MTK", 1_000 ether);
+        StakingVault vault = new StakingVault(address(token));
+
+        vm.stopBroadcast();
+
+        console2.log("MyToken deployed at:", address(token));
+        console2.log("StakingVault deployed at:", address(vault));
+    }
+}
